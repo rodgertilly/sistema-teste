6aed12a8b70697a74d7d4a920aaa2d7b5148062a
Rodrigo Carvalho <rodger2@gmail.com>
2022-11-10 16:03:57 -0300

    adicionando o desafio do segundo dia

---
+++ 7 Days Of Code/7days2.js
@@ -0,0 +1,12 @@
+const nome = prompt("Qual o seu nome?");
+const idade = prompt("Quantos anos você tem?");
+const linguagem = prompt("Qual linguagem de programação você está aprendendo?");
+const msg = `Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`;
+alert(msg);
+const resposta = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO.`)
+if (resposta == 1) {
+    alert("Muito bom! Continue estudando e você terá muito sucesso.")
+}
+if (resposta == 2) {
+    alert("Ahh que pena... Já tentou aprender outras linguagens?")
+}
